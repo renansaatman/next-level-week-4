fe71e6b1599ec28725c36ae6b2e1941434073667
Renan Saatman <renansaatman@hotmail.com>
2021-02-23 16:44:36 -0300

    add countdown function

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,6 +9,10 @@ import styles from '../styles/pages/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Início | move.it</title>
+      </Head>
+
       <ExperienceBar />
 
       <section>
